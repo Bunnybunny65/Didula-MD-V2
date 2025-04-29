1319b301fbb942729457ef31ecec71475e05be55
Bunnybunny65 <bunnythiwa4@gmail.com>
2025-04-29 14:27:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
+STATUS_READ_MSG: process.env.STATUS_READ_MSG || "ʙᴜɴɴʏ 𝗦𝘁𝗮𝘁𝘂𝘀 𝗞𝗜𝗡𝗚",
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
-FOOTER: process.env.FOOTER || "*◆─〈 ✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦ 〉─◆*",
+FOOTER: process.env.FOOTER || "*◆─〈 ✦ʙᴜɴɴʏ=ᴍᴅ 〉─◆*",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
+IMAGE_LIMIT: process.env.IMAGE_LIMIT || "5",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~pq9XVSxZ#Qenat052FjMN3acoIfqGfR8xiPsVjZUSEYPvj_QPU9Q",
 };
 
 
